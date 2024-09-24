c89c79e9825af35d1965a20375fadaf507ad9b5c
unknown <namwmambaobuyu25@gmail.com>
2024-09-24 15:11:38 +0300

    second commit

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class BottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  BottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      child: GNav(
+          color: Colors.brown[400],
+          activeColor: Colors.brown[900],
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabBackgroundColor: Colors.white,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 8,
+          onTabChange: (value) => onTabChange!(value),
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Shop',
+            ),
+            GButton(
+              icon: Icons.shopping_bag_rounded,
+              text: 'Cart',
+            ),
+            GButton(
+              icon: Icons.person,
+              text: 'Profile',
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: 'Settings',
+            )
+          ]),
+    );
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:winnastores/components/bottom_nav_bar.dart';
+import 'package:winnastores/pages/profile_page.dart';
+import 'package:winnastores/pages/setting_page.dart';
+import 'package:winnastores/pages/shop_page.dart';
+
+import 'cart_page.dart';
+
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
@@ -7,10 +14,35 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  //this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+  //this method will update our selected index
+  //when user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+//pages to display
+  final List<Widget> _pages = [
+    //shop page
+    const ShopPage(),
+    //cart page
+    const CartPage(),
+    //profile page
+    const ProfilePage(),
+    //settings page
+    const SettingPage()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.brown[100],
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
